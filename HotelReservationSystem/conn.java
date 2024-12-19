613c91597e6778fae85b904f4b760307fe200ba0
Anmol Gupta <anmollgupta9105@gmail.com>
2024-12-19 23:51:50 +0530

    First Commit

--- HotelReservationSystem/conn.java
+++ HotelReservationSystem/conn.java
@@ -2,18 +2,19 @@ package HotelReversationSystem.HotelReservationSystem;
 
 import java.sql.*;
 
-    public class conn{
+    public class conn {
         Connection c;
         Statement s;
-        public conn(){
-            try{
+
+        public conn() {
+            try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
-                c =DriverManager.getConnection("jdbc:mysql://localhost:3306/hotelreservation","root","root123");
+                c = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotelreservation", "root", "root123");
 
-                s =c.createStatement();
+                s = c.createStatement();
 
 
-            }catch(Exception e){
+            } catch (Exception e) {
 //                System.out.println(e);
             }
         }
